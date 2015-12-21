b93666d3fc604fe685a4719c3bb7cbcfb7e8d993
Ed Ng <ed.jk.ng@gmail.com>
2015-12-20 18:01:02 -0800

    Moving Buttonwood Files Under Its Own Directory

---
+++ buttonwood/bot.js
@@ -0,0 +1,80 @@
+var Botkit = require('./node_modules/botkit/lib/Botkit.js');
+var yahooFinance = require('yahoo-finance');
+
+var controller = Botkit.slackbot({
+  debug: false,
+});
+
+var bot = controller.spawn(
+  {
+    token:process.env.token
+  }
+).startRTM();
+
+
+controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot,message) {
+
+  bot.api.reactions.add({
+    timestamp: message.ts,
+    channel: message.channel,
+    name: 'robot_face',
+  },function(err,res) {
+    if (err) {
+      bot.botkit.log("Failed to add emoji reaction :(",err);
+    }
+  });
+
+
+  controller.storage.users.get(message.user,function(err,user) {
+    if (user && user.name) {
+      bot.reply(message,"Hello " + user.name+"! I am buttonwood.");
+    } else {
+      bot.reply(message,"Hello. I am buttonwood.");
+    }
+  });
+});
+
+controller.hears(['call me (.*)'],'direct_message,direct_mention,mention',function(bot,message) {
+  var matches = message.text.match(/call me (.*)/i);
+  var name = matches[1];
+  controller.storage.users.get(message.user,function(err,user) {
+    if (!user) {
+      user = {
+        id: message.user,
+      };
+    }
+    user.name = name;
+    controller.storage.users.save(user,function(err,id) {
+      bot.reply(message,"Got it. I will call you " + user.name + " from now on.");
+    });
+  });
+});
+
+controller.hears(['what is my name','who am i'],'direct_message,direct_mention,mention',function(bot,message) {
+
+  controller.storage.users.get(message.user,function(err,user) {
+    if (user && user.name) {
+      bot.reply(message,"Your name is " + user.name);
+    } else {
+      bot.reply(message,"I don't know yet!");
+    }
+  });
+});
+
+function formatUptime(uptime) {
+  var unit = 'second';
+  if (uptime > 60) {
+    uptime = uptime / 60;
+    unit = 'minute';
+  }
+  if (uptime > 60) {
+    uptime = uptime / 60;
+    unit = 'hour';
+  }
+  if (uptime != 1) {
+    unit = unit +'s';
+  }
+
+  uptime = uptime + ' ' + unit;
+  return uptime;
+}
