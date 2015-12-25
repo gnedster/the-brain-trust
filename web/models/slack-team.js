f7ee050973f3ddb21b6a43f4e88fbabe04cca914
Ed Ng <ed.jk.ng@gmail.com>
2015-12-24 19:32:06 -0800

    Slack Id should be unique

--- web/models/slack-team.js
+++ web/models/slack-team.js
@@ -9,7 +9,8 @@ var SlackTeam = sequelize.define('SlackTeam', {
   slackId: {
     type: Sequelize.STRING,
     allowNull : false,
-    field: 'slack_id'
+    field: 'slack_id',
+    unique: true
   },
   slackName: {
     type: Sequelize.STRING,
