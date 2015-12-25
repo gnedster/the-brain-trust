c3e2e21707aaeb82c1d35813303e53d96a9b0913
Ed Ng <ed.jk.ng@gmail.com>
2015-12-24 19:16:37 -0800

    Obscuring Slack Team Id

--- web/test/lib/fake-oauth.js
+++ web/test/lib/fake-oauth.js
@@ -33,15 +33,15 @@ function handleRequest(request, response){
         ok: true,
         access_token: 'xoxp-' + Math.floor(Math.random() * 100),
         scope: 'identify,incoming-webhook,bot',
-        team_name: 'The Brain Trust',
-        team_id: 'T0GGVD0NS',
+        team_name: 'team name',
+        team_id: 'team id',
         incoming_webhook: {
           channel: '#cheese',
           configuration_url: 'https://opnd.slack.com/services/1',
           url: 'https://hooks.slack.com/services/2/2/35134124312'
         },
         bot: {
-          bot_user_id: '4421',
+          bot_user_id: '123',
           bot_access_token: 'xoxb-' + Math.floor(Math.random() * 100),
         }
       }));
