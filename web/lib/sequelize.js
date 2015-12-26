7d77403cf7b6b7377934a3a139e9c51bcf84a9bc
Ed Ng <ed.jk.ng@gmail.com>
2015-12-26 10:42:06 -0800

    Combining Database Env Variables

--- web/lib/sequelize.js
+++ web/lib/sequelize.js
@@ -9,13 +9,9 @@ var rdsConfig = config.get('rds');
 var Sequelize = require('sequelize');
 var util = require('./util');
 
-if (util.isProduction() === true) {
+if (util.isProduction() === true || util.isTest() === true) {
   rdsConfig.username = process.env.RDS_USERNAME;
   rdsConfig.password = process.env.RDS_PASSWORD;
-} else if (util.isTest() === true) {
-  // https://codeship.com/documentation/databases/postgresql/
-  rdsConfig.username = process.env.PG_USER;
-  rdsConfig.password = process.env.PG_PASSWORD;
 }
 
 logger.debug('starting rds connection with config:\n' +
