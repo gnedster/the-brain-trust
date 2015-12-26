43ac0ed0a64a14919a5a74f87b2a40573837d39e
Ed Ng <ed.jk.ng@gmail.com>
2015-12-26 08:20:13 -0800

    Adding First Suite of Tests

---
+++ web/test/buttonwood_test.js
@@ -0,0 +1,54 @@
+/**
+ * Add tests for buttonwood
+ */
+
+var app = require('../app');
+var assert = require('assert');
+var config = require('config');
+var db = require('../db/db');
+var request = require('supertest');
+
+describe('GET /buttonwood', function(){
+  it('respond with html', function(done){
+    request(app)
+      .get('/buttonwood')
+      .set('Accept', 'application/html')
+      .expect('Content-Type', /html/)
+      .expect(200, /buttonwood/, done);
+  });
+});
+
+describe('GET /buttonwood/authorize', function(){
+  it('responds with 404 without valid params', function(done){
+    request(app)
+      .get('/buttonwood/authorize')
+      .set('Accept', 'application/html')
+      .expect(404, /not found/, done);
+  });
+
+  it('responds with 200, unauthorized on access_denied', function(done){
+    request(app)
+      .get('/buttonwood/authorize?error=access_denied&state=' +
+        config.get('oauth.state'))
+      .set('Accept', 'application/html')
+      .expect(200, /whoa/, done);
+  });
+
+  it('responds with 200, error on any other error', function(done){
+    request(app)
+      .get('/buttonwood/authorize?error=some_error&state=' +
+        config.get('oauth.state'))
+      .set('Accept', 'application/html')
+      .expect(200, /hmm/, done);
+  });
+
+  /**
+   * Fake OAuth server needs to be up.
+   */
+  it('responds with 200 with valid code and state', function(done){
+    request(app)
+      .get('/buttonwood/authorize?code=1&state=' + config.get('oauth.state'))
+      .set('Accept', 'application/html')
+      .expect(200, /you\'re in/, done);
+  });
+});
---
+++ web/test/index_test.js
@@ -0,0 +1,36 @@
+/**
+ * Add tests for the basic website
+ */
+
+var app = require('../app');
+var assert = require('assert');
+var request = require('supertest');
+
+describe('GET /', function(){
+  it('respond with html', function(done){
+    request(app)
+      .get('/')
+      .set('Accept', 'application/html')
+      .expect('Content-Type', /html/)
+      .expect(200, /the brain trust/, done);
+  });
+});
+
+describe('GET /privacy-policy', function(){
+  it('respond with OK', function(done){
+    request(app)
+      .get('/')
+      .set('Accept', 'application/html')
+      .expect('Content-Type', /html/)
+      .expect(200, /privacy policy/, done);
+  });
+});
+
+describe('GET /health', function(){
+  it('respond with OK', function(done){
+    request(app)
+      .get('/health')
+      .set('Accept', 'application/*')
+      .expect(200, 'OK', done);
+  });
+});
