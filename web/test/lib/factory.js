d72c2d2c6168abf9ebc65710d45a132921044778
Ed Ng <ed.jk.ng@gmail.com>
2015-12-31 16:15:32 -0800

    ApplicationPlatform No Longer Stores Json

--- web/test/lib/factory.js
+++ web/test/lib/factory.js
@@ -22,10 +22,8 @@ factory.define('application', rds.models.Application, {
 factory.define('application-permission', rds.models.ApplicationPlatform, {
   application_id: factory.assoc('application', 'id'),
   platform_id: factory.assoc('platform', 'id'),
-  credentials: {
-    clientId: faker.random.uuid(),
-    clientToken: faker.random.uuid()
-  }
+  token: faker.random.uuid(),
+  clientId: faker.random.uuid()
 });
 
 module.exports = factory;
