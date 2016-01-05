28ac7412b4e01b712ee01453f11926d97f5c62df
Terence Hampson <terence@the-brain-trust.com>
2016-01-05 02:40:16 -0500

    sqs: reorder require list

--- sqs/test/sqs-test.js
+++ sqs/test/sqs-test.js
@@ -1,8 +1,8 @@
 /**
  * Add tests for the rds module.
  */
-var assert = require('assert');
 var _ = require('lodash');
+var assert = require('assert');
 var sqs = require('../sqs');
 var specHelper = require('./lib/spec-helper');
 
