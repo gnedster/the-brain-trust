3b719fab84f434e5ff4c5898a348b6e76660aff9
Ed Ng <ed.jk.ng@gmail.com>
2016-01-09 09:06:55 -0800

    Fix Tests

--- buttonwood/test/routes-buttonwood-test.js
+++ buttonwood/test/routes-buttonwood-test.js
@@ -30,6 +30,7 @@ describe('/buttonwood', function() {
           user_id: faker.random.uuid()
         })
         .expect('Content-Type', 'application/json')
+        .expect(200)
         .end(function(err, res) {
           assert(res);
           done();
@@ -59,7 +60,10 @@ describe('/buttonwood', function() {
         })
         .expect(200, /valid/, done);
     });
+  }
 
+  function shouldNotBeFoundWithoutToken(command) {
+    command = command ? '_' + command : '';
     it('responds 404 without token', function(done){
       request(app)
         .post(`/buttonwood/commands/quote${command}`)
@@ -93,21 +97,39 @@ describe('/buttonwood', function() {
 
   describe('POST commands/quote', function(){
     shouldRespondToQuoteRequest();
+    shouldNotBeFoundWithoutToken();
   });
 
   describe('POST commands/quote_detailed', function(){
     shouldRespondToQuoteRequest('detailed');
+    shouldNotBeFoundWithoutToken('detailed');
   });
 
   describe('POST commands/quote_add', function(){
     shouldRespondToQuoteRequest('add');
+    shouldNotBeFoundWithoutToken('add');
   });
 
   describe('POST commands/quote_remove', function(){
     shouldRespondToQuoteRequest('remove');
+    shouldNotBeFoundWithoutToken('remove');
   });
 
   describe('POST commands/quote_list', function(){
-    shouldRespondToQuoteRequest('list');
+    it('responds with OK with token and no saved quotes', function(done){
+      request(app)
+        .post('/buttonwood/commands/quote_list')
+        .set('Accept', 'application/json')
+        .type('form')
+        .send({
+          token: commandToken,
+          team_id: faker.random.uuid(),
+          channel_id: faker.random.uuid(),
+          user_id: faker.random.uuid()
+        })
+        .expect(200, /quote_add/, done);
+    });
+
+    shouldNotBeFoundWithoutToken('list');
   });
 });
