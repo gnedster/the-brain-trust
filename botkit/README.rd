eb724fff66d33575c2fc29c6f037a1c64904d074
Terence Hampson <terence@the-brain-trust.com>
2016-01-12 23:59:53 -0500

    botkit: Initial botkit fork from version 0.0.5
    Enable testing of braintrust application.

---
+++ botkit/README.rd
@@ -0,0 +1,13 @@
+# botkit
+
+**private**
+
+Provides the botkit for connecting to slack
+
+## Usage
+
+`var botkit = require ('@the-brain-trust/botkit');`
+
+## Publishing
+
+`npm publish`
---
+++ botkit/test/Slack_web_api-test.js
@@ -0,0 +1,69 @@
+var test = require('tape');
+var Botkit = require('../');
+var env = require('node-env-file');
+var logger = require('@the-brain-trust/logger');
+var path = require('path');
+
+env(path.join(__dirname, '..', '.env'));
+var token = {token:process.env.TOKEN};
+
+test('sanity', t=> {
+  t.plan(4);
+  t.ok(token, '.env sets TOKEN');
+  logger.info(token);
+  t.ok(Botkit, 'Botkit exists');
+  t.ok(Botkit.core, 'Botkit.core exists');
+  t.ok(Botkit.slackbot, 'Botkit.slackbot exists');
+  logger.info(Botkit);
+});
+
+test('can start and then stop a bot', t=> {
+
+  var controller = Botkit.slackbot({debug:false});
+  t.plan(3);
+
+  controller.on('rtm_open',function(bot) {
+    t.ok(bot,'rtm_open fired');
+  });
+
+  controller.on('rtm_close',function(bot) {
+
+    t.ok(bot,'disconnected successfully');
+
+    controller.shutdown();
+    t.end();
+
+  });
+
+  controller.spawn(token).startRTM((err, bot, payload)=> {
+
+    if (err) {
+      t.fail(err, err);
+    }
+    else {
+      t.ok(bot, 'got the bot');
+      logger.info(Object.keys(bot));
+      bot.closeRTM();
+
+    }
+
+  });
+});
+
+test('failed bot properly fails',t=>{
+
+  var controller = Botkit.slackbot({debug:false});
+  controller.spawn('1231').startRTM((err,bot,payload)=>{
+
+    if (err) {
+      t.ok(err,'got an error');
+      logger.error(err);
+    } else {
+      t.fail('Should have errored!','Should have errored!');
+    }
+    controller.shutdown();
+    t.end();
+
+  });
+
+});
