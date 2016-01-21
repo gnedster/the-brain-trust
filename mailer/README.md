6cc25e36d089486f378b173fedc985c808101e3e
Ed Ng <ed.jk.ng@gmail.com>
2016-01-20 23:37:56 -0800

    Mailer Becomes a Seperate Module

---
+++ mailer/README.md
@@ -0,0 +1,13 @@
+# mailer
+
+**private**
+
+Provides a common mailer module. Backed by AWS SDK.
+
+## Usage
+
+`var mailer = require('@the-brain-trust/mailer');`
+
+## Publishing
+
+`npm publish`
---
+++ mailer/test/mailer-test.js
@@ -0,0 +1,45 @@
+/**
+ * Add tests for the rds module.
+ */
+var assert = require('assert');
+var faker = require('faker');
+var mailer = require('../mailer');
+var maildev = require('maildev')();
+
+describe('mailer', function(){
+
+  before(function(done) {
+    maildev.listen(function(err){
+      if (typeof err === 'undefined') {
+        done();
+      }
+    });
+  });
+
+  it('should initialize', function(done){
+    var subject = faker.lorem.sentence();
+    maildev.on('new', function(email){
+      assert(subject, email.subject);
+      done();
+    });
+
+    mailer.sendEmail({
+      Destination: {
+        ToAddresses: [faker.internet.email()]
+      },
+      Message: {
+        Body: {
+          Html: {
+            Data: faker.lorem.paragraphs()
+          },
+          Text: {
+            Data: faker.lorem.paragraphs()
+          }
+        },
+        Subject: {
+          Data: subject
+        }
+      }
+    });
+  });
+});
