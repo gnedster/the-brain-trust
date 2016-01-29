723f90b85b6840cd819c85472f62c4fbb7b038f6
Ed Ng <ed.jk.ng@gmail.com>
2016-01-28 21:06:00 -0800

    Add Symbol Data

---
+++ buttonwood/rds/models/symbol.js
@@ -0,0 +1,37 @@
+var rds = require('@the-brain-trust/rds');
+var Sequelize = require('sequelize');
+
+/**
+ * Available symbols
+ * @type {Model}
+ */
+var Symbol = rds.define('Symbol', {
+  ticker: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  exchange: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  country: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  categoryName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  categoryNumber: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+}, {
+  paranoid: false
+});
+
+module.exports = Symbol;
