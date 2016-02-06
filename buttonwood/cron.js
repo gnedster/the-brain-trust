a6699e1dad8de6f3f41069febb1717cd2dd4bc83
Ed Ng <ed.jk.ng@gmail.com>
2016-02-06 12:43:20 -0800

    Update Cron Job

--- buttonwood/cron.js
+++ buttonwood/cron.js
@@ -3,15 +3,22 @@ var CronJob = require('cron').CronJob;
 var buttonwood = require('./app/buttonwood');
 var botManager = require('./lib/bot-manager');
 
+/**
+ * Push portfolio summaries to users
+ */
+function pushSummaries() {
+  buttonwood.getPortfolioSummaries().then(function(portfolioSummaries) {
+    _.each(portfolioSummaries, function(portfolioSummary) {
+      var bot = botManager.getBot(portfolioSummary.applicationPlatformEntity);
+      bot.sendPrivateMessage(portfolioSummary);
+    });
+  });
+}
+
 function init() {
   // Push summaries at 4:20 PM ET every weekday.
-  new CronJob('00 16 20 * * 1-5',
-    function() {
-      _.each(buttonwood.getPortfolioSummaries(), function(portfolioSummary) {
-        var bot = botManager.getBot(portfolioSummary.applicationPlatformEntity);
-        bot.sendPrivateMessage(portfolioSummary);
-      });
-    },
+  new CronJob('00 20 16 * * 1-5',
+    pushSummaries,
     null,
     true,
     'America/New_York');
