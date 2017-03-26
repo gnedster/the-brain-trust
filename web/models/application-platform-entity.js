2ae7bd6f8a8383ed4b1ce90ffe8d34f8d2858a24
Ed Ng <ed.jk.ng@gmail.com>
2017-03-26 14:17:54 -0700

    Remove npmjs + SQS Dependency

--- web/models/application-platform-entity.js
+++ web/models/application-platform-entity.js
@@ -1,23 +1,12 @@
 var mailers = require('../mailers/registry');
 var rds = require('@the-brain-trust/rds');
-var sqs = require('@the-brain-trust/sqs');
 
 
 rds.models.ApplicationPlatformEntity
   .hook('afterCreate', function(applicationPlatformEntity, options) {
-    sqs.sendInstanceMessage(
-      'application',
-      'application-platform-entity created',
-      applicationPlatformEntity);
-
     mailers.newAuthorization(applicationPlatformEntity);
   })
   .hook('afterUpdate', function(applicationPlatformEntity, options) {
-    sqs.sendInstanceMessage(
-      'application',
-      'application-platform-entity updated',
-      applicationPlatformEntity);
-
     mailers.newAuthorization(applicationPlatformEntity);
   });
 
